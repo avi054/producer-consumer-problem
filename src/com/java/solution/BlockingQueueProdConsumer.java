221f49363d39103fbd1bdc9cf3eef49273196b14
Aviral Agarwal <aviralagarwal.2013@gmail.com>
2021-06-19 22:23:13 +0530

    clean up

--- src/com/java/solution/BlockingQueueProdConsumer.java
+++ src/com/java/solution/BlockingQueueProdConsumer.java
@@ -8,27 +8,13 @@ public class BlockingQueueProdConsumer {
 		//queue with fixed size of 10
 		BlockingQueue<Employee> queue = new ArrayBlockingQueue<>(10);
 		
-		Producer producer = new Producer(queue);//fast producer
-//		SlowProducer producer2 = new SlowProducer(queue);
-		
+		Producer producer = new Producer(queue);
 		Consumer consumer = new Consumer(queue);
-//		SlowConsumer slowConsumer = new SlowConsumer(queue);
 		
 		Thread producerThread = new Thread(producer);
-		
-//		Thread slowProducerThread = new Thread(producer2);
-		
 		Thread consumerThread = new Thread(consumer);
-		
-//		Thread slowConsumerThread = new Thread(slowConsumer);
 
 		producerThread.start();
-		
-//		slowProducerThread.start();// slow producer with delay of 5ms
-		
-		consumerThread.start(); // fast consumer
-		
-//		slowConsumerThread.start(); // slow consumer with delay of 5ms
-
+		consumerThread.start();
 	}
 }
